0c41886decb9ffbe080924b16c199fc29688e990
root <avash.shakya@protonmail.com>
2024-10-18 16:31:49 +0545

    new

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,10 @@ app.get("/server-health", (_req: Request, res: Response) => {
   res.json({ value: "api is working" });
 });
 
+app.get("/new", (_req: Request, res: Response) => {
+  res.json({ value: "New features coming soon!!!" });
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
